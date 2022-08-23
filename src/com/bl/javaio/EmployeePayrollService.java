a6458dd1905d24f217627bb8bd1faf1f6002d030
Darpan <darpan.madlani@gmail.com>
2022-08-23 20:10:49 +0530

    Initial commit

---
+++ src/com/bl/javaio/EmployeePayrollService.java
@@ -0,0 +1,13 @@
+package com.bl.javaio;
+
+public class EmployeePayrollService {
+
+	public enum IOStream {
+		CONSOLE_IO, FILE_IO, DB_IO
+	}
+
+	public static void main(String[] args) {
+		IOStream iOStream = IOStream.CONSOLE_IO;
+		
+	}		
+}
